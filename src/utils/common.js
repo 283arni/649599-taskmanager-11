b40da1c199b16d8a6699d5762f444ab4eca2c1a6
Aleksandr Arnautov <283arni@gmail.com>
2020-04-21 22:26:06 +0300

    fix

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,5 @@
 import moment from "moment";
 
-const castTimeFormat = (value) => {
-  return value < 10 ? `0${value}` : String(value);
-};
-
 export const formatTime = (date) => {
   return moment(date).format(`HH:mm`);
 };
